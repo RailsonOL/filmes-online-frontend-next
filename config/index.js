40ae6d25dde82bc8f695073424dc6dd08e2bdd30
Railson OL <itsrayplay@gmail.com>
2021-02-06 18:19:58 -0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'http://localhost:3000' : `https://${NEXT_PUBLIC_VERCEL_URL}`
+export const server = dev ? 'http://localhost:3000' : `https://amazoflix-git-server-side-render.railsonol.vercel.app`
