4c8ce9dcd4b4cc1114cfa92e90bf6f5aaf95f4de
Railson OL <itsrayplay@gmail.com>
2021-03-11 17:58:26 -0300

    Update geturl.js

--- pages/api/geturl.js
+++ pages/api/geturl.js
@@ -1,10 +1,17 @@
 import axios from 'axios'
 import { responseJson } from '../../utils'
 
-const get = async (res, req) => {
-  // https://cors-anywhere.herokuapp.com/https://api.animesgratisbr.com/video/16788
+const get = async (req, res) => {
   const { data } = await axios.get('https://api.animesgratisbr.com/video/16788')
-  return responseJson(res, data)
+
+  return responseJson(res, { data })
 }
 
 export default get
+
+// https://cors-anywhere.herokuapp.com/https://api.animesgratisbr.com/video/16788
+//   const { data } = await axios.get('https://cors-anywhere.herokuapp.com/https://api.animesgratisbr.com/video/16788', {
+//     headers: {
+//       origin: 'https://api.animesgratisbr.com/video/16788'
+//     }
+//   })
